879f7bf7689a017f1e9d2193ee7547712ec647e1
axa797 <117784608+axa797@users.noreply.github.com>
2026-04-28 08:45:20 -0700

    chore(web): ignore generated Next.js env stubs after churn

--- web/next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// Note: run `npm run dev` or `npm run build` to generate `.next/types` if your IDE
-// expects route types.
